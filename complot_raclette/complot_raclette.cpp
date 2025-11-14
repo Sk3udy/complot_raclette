b1f557f75c20d5e2a169065276f63aab02c443ab
avaesken <avaesken@gaming.tech>
2025-11-14 12:24:15 +0100

    Change image

--- complot_raclette/complot_raclette.cpp
+++ complot_raclette/complot_raclette.cpp
@@ -1,16 +1,21 @@
 #include "framework.h"
 #include "complot_raclette.h"
 #include <windows.h>
-
-#include <windows.h>
 #include <tchar.h>
 #include <vector>
+#include <commdlg.h>
+#define ID_BUTTON_CREER    101
+#define ID_BUTTON_QUITTER  102
+
+#define ID_MENU_CHARGER    201
+#define ID_MENU_QUITTER    202
+#define ID_MENU_APROPOS    203
 
 using namespace std;
 
 // --- Variables globales ---
-int g_width = 600;
-int g_height = 400;
+int g_width = 2100;
+int g_height = 1200;
 int g_colorIndex = 0;
 HBRUSH g_hBrush = NULL;
 
@@ -32,13 +37,38 @@ HMENU Mebar;
 HMENU MeFile;
 HMENU MeHelp;
 
+HBITMAP hBitmap = (HBITMAP)LoadImage(
+    NULL, L"C:\\Users\\avaesken\\Pictures\\raclette_1080p.bmp",
+    IMAGE_BITMAP, 0, 0,
+    LR_LOADFROMFILE | LR_CREATEDIBSECTION
+
+);
+
+void PaintBitmap(HDC hdc, HBITMAP hBmp) {
+    HDC hdcMem = CreateCompatibleDC(hdc);
+
+    // Sauvegarde l’ancien bitmap avant de mettre le nouveau
+    HBITMAP hOldBmp = (HBITMAP)SelectObject(hdcMem, hBmp);
+
+    BITMAP bmp;
+    GetObject(hBmp, sizeof(BITMAP), &bmp);
+
+    // Copie le bitmap dans la fenêtre
+    BitBlt(hdc, 0, 0, bmp.bmWidth, bmp.bmHeight, hdcMem, 0, 0, SRCCOPY);
+
+    // Restauration de l’ancien bitmap
+    SelectObject(hdcMem, hOldBmp);
+
+    DeleteDC(hdcMem);
+}
+
 // --- Fenêtre callback ---
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
     switch (msg)
     {
     case WM_CREATE:
-        g_hBrush = CreateSolidBrush(RGB(0, 0, 255));
+        g_hBrush = CreateSolidBrush(RGB(240, 240, 240));
         SetWindowText(hwnd, _T("Fenêtre initiale"));
 
 
@@ -50,184 +80,130 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 
         CreateWindowEx(0, _T("BUTTON"), _T("Créer"),
             WS_CHILD | WS_VISIBLE,
-            20, 60, 80, 25, hwnd, (HMENU)1, ((LPCREATESTRUCT)lParam)->hInstance, NULL);
+            20, 60, 80, 25, hwnd, (HMENU)ID_BUTTON_CREER, ((LPCREATESTRUCT)lParam)->hInstance, NULL);
 
         CreateWindowEx(0, _T("BUTTON"), _T("Quitter"),
             WS_CHILD | WS_VISIBLE,
-            120, 60, 80, 25, hwnd, (HMENU)2, ((LPCREATESTRUCT)lParam)->hInstance, NULL);
+            120, 60, 80, 25, hwnd, (HMENU)ID_BUTTON_QUITTER, ((LPCREATESTRUCT)lParam)->hInstance, NULL);
 
-        CreateWindowEx(0, _T("BUTTON"), _T("Quitter"),
-            WS_CHILD | WS_VISIBLE,
-            120, 60, 80, 25, hwnd, (HMENU)3, ((LPCREATESTRUCT)lParam)->hInstance, NULL);
 
+
+      
 
         Mebar = CreateMenu();
         MeFile = CreateMenu();
         MeHelp = CreateMenu();
 
-        AppendMenu(MeFile, MF_STRING, 2, _T("tu leave pas"));
-        AppendMenu(MeHelp, MF_STRING, 1, _T("A propos (non, t'aura rien)"));
-        AppendMenu(MeHelp, MF_STRING, 3, _T("fond"));
-
-        AppendMenu(Mebar, MF_POPUP, (UINT_PTR)MeFile, _T("un truc jaune"));
-        AppendMenu(Mebar, MF_POPUP, (UINT_PTR)MeHelp, _T("Aide??? y'a pas"));
+        AppendMenu(MeFile, MF_STRING, ID_MENU_CHARGER, _T("Charger une image: "));
+        AppendMenu(MeFile, MF_STRING, ID_MENU_QUITTER, _T("Quitter. "));
+        AppendMenu(MeHelp, MF_STRING, ID_MENU_APROPOS, _T("A propos. "));
+    
+        AppendMenu(Mebar, MF_POPUP, (UINT_PTR)MeFile, _T("Fichier "));
+        AppendMenu(Mebar, MF_POPUP, (UINT_PTR)MeHelp, _T("Aide "));
 
         SetMenu(hwnd, Mebar);
 
 
         break;
 
-    case WM_SIZE:
-        g_width = LOWORD(lParam);
-        g_height = HIWORD(lParam);
+    case WM_PAINT: {
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(hwnd, &ps);
 
-        {
-            TCHAR title[100];
-            wsprintf(title, _T("Fenêtre - %dx%d"), g_width, g_height);
-            SetWindowText(hwnd, title);
-        }
-        break;
+        if (hBitmap) {
+            HDC hdcMem = CreateCompatibleDC(hdc);
+            HBITMAP hOldBmp = (HBITMAP)SelectObject(hdcMem, hBitmap);
 
-    case WM_SETCURSOR:
-    {
-        POINT pt;
-        GetCursorPos(&pt);
-        ScreenToClient(hwnd, &pt);
-        if (pt.x < g_width / 2)
-            SetCursor(LoadCursor(NULL, IDC_HAND));
-        else
-            SetCursor(LoadCursor(NULL, IDC_CROSS));
-        return TRUE;
-    }
-    case WM_LBUTTONDOWN:
-    {
-        int x = LOWORD(lParam);
-        int y = HIWORD(lParam);
+            BITMAP bmp;
+            GetObject(hBitmap, sizeof(BITMAP), &bmp);
 
-        // Déplacement de la fenêtre
-        g_bDragging = TRUE;
-        g_ptOffset = { x, y };
-        SetCapture(hwnd);
+            // Récupère la taille de la fenêtre
+            RECT rc;
+            GetClientRect(hwnd, &rc);
 
-        // Dessin à main levée
-        g_bDrawing = TRUE;
-        g_points.push_back({ x, y });
+            // Étire l’image pour remplir la fenêtre
+            SetStretchBltMode(hdc, HALFTONE);
+            StretchBlt(
+                hdc,
+                0, 0, rc.right, rc.bottom,   // destination : toute la fenêtre
+                hdcMem,
+                0, 0, bmp.bmWidth, bmp.bmHeight, // source : taille originale du BMP
+                SRCCOPY
+            );
 
-        // Changement de couleur dynamique
-        g_colorIndex = (g_colorIndex + 1) % 3;
-        COLORREF c = (g_colorIndex == 0) ? RGB(255, 0, 0) :
-            (g_colorIndex == 1) ? RGB(0, 255, 0) : RGB(0, 0, 255);
-        if (g_hBrush)
-            DeleteObject(g_hBrush);
-        g_hBrush = CreateSolidBrush(c);
-
-        InvalidateRect(hwnd, NULL, TRUE);
-        break;
-    }
-
-
-
-
-    case WM_MOUSEMOVE:
-    {
-        int x = LOWORD(lParam);
-        int y = HIWORD(lParam);
-
-        if (g_bDragging)
-        {
-            POINT ptScreen = { x, y };
-            ClientToScreen(hwnd, &ptScreen);
-            SetWindowPos(hwnd, NULL, ptScreen.x - g_ptOffset.x, ptScreen.y - g_ptOffset.y,
-                0, 0, SWP_NOSIZE | SWP_NOZORDER);
+            // Restaure l’ancien bitmap
+            SelectObject(hdcMem, hOldBmp);
+            DeleteDC(hdcMem);
         }
 
-        if (g_bDrawing)
-        {
-            g_points.push_back({ x, y });
-            InvalidateRect(hwnd, NULL, FALSE);
-        }
-    }
-    break;
+        EndPaint(hwnd, &ps);
+    } break;
 
-    case WM_LBUTTONUP:
-        g_bDragging = FALSE;
-        g_bDrawing = FALSE;
-        ReleaseCapture();
-        break;
-
-    case WM_KEYDOWN:
-    {
-        TCHAR keyName[32] = _T("");
-        LONG lParamCopy = (lParam & 0x01FF0000) | ((lParam & 0x0000FFFF) << 16);
-        GetKeyNameText(lParamCopy, keyName, 32);
-        SetWindowText(hwnd, keyName);
-
-        if (wParam == VK_ESCAPE)
-            PostQuitMessage(0);
-    }
-    break;
-
-    case WM_ERASEBKGND:
-    {
-        HDC hdc = (HDC)wParam;
-        RECT rc;
-        GetClientRect(hwnd, &rc);
-        FillRect(hdc, &rc, g_hBrush);
-        return 1; // effacement géré
+    case WM_ERASEBKGND:{
+        return 0; 
     }
 
     case WM_COMMAND:
     {
-        if (LOWORD(wParam) == 1) // Créer
+        switch (LOWORD(wParam)){
+        case ID_BUTTON_CREER:
         {
+            
             TCHAR buffer[256];
             GetWindowText(hEdit, buffer, 256);
-            MessageBox(hwnd, buffer, _T("Texte saisi"), MB_OK);
+            MessageBox(hwnd, buffer, _T("Texte Saisi"), MB_OK);
         }
-        else if (LOWORD(wParam) == 2) // Quitter
-        {
+        break;
+
+       case ID_BUTTON_QUITTER:
+
             PostQuitMessage(0);
-        }
-    }
-    break;
+            break;
 
-    case WM_PAINT:
+        case ID_MENU_CHARGER: // Charger une image
     {
-        PAINTSTRUCT ps;
-        HDC hdc = BeginPaint(hwnd, &ps);
+        OPENFILENAME ofn;
+        TCHAR szFile[MAX_PATH] = _T("");
 
-        // Rectangle rouge centré
-        int rectWidth = 200;
-        int rectHeight = 150;
-        RECT rect = {
-            g_width / 2 - rectWidth / 2,
-            g_height / 2 - rectHeight / 2,
-            g_width / 2 + rectWidth / 2,
-            g_height / 2 + rectHeight / 2
-        };
+        ZeroMemory(&ofn, sizeof(ofn));
+        ofn.lStructSize = sizeof(ofn);
+        ofn.hwndOwner = hwnd;
+        ofn.lpstrFile = szFile;
+        ofn.nMaxFile = MAX_PATH;
+        ofn.lpstrFilter = _T("Bitmap Files/0*.bmp/0All Files/0*.*/0");
+        ofn.nFilterIndex = 1;
+        ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST;
 
-        HBRUSH hBrushRed = CreateSolidBrush(RGB(255, 0, 0));
-        FillRect(hdc, &rect, hBrushRed);
-        DeleteObject(hBrushRed);
-
-        // Dessin à main levée
-        if (g_points.size() > 1)
-        {
-            HPEN hPen = CreatePen(PS_SOLID, 2, RGB(0, 0, 0));
-            HGDIOBJ oldPen = SelectObject(hdc, hPen);
-            MoveToEx(hdc, g_points[0].x, g_points[0].y, NULL);
-            for (size_t i = 1; i < g_points.size(); i++)
-            {
-                LineTo(hdc, g_points[i].x, g_points[i].y);
+        if (GetOpenFileName(&ofn)) {
+            // Libérer l’ancien bitmap si nécessaire
+            if (hBitmap) {
+                DeleteObject(hBitmap);
             }
-            SelectObject(hdc, oldPen);
-            DeleteObject(hPen);
-        }
 
-        EndPaint(hwnd, &ps);
+            // Charger le nouveau
+            hBitmap = (HBITMAP)LoadImage(
+                NULL, ofn.lpstrFile,
+                IMAGE_BITMAP, 0, 0,
+                LR_LOADFROMFILE | LR_CREATEDIBSECTION
+            );
+
+            if (!hBitmap) {
+                MessageBox(hwnd, _T("Impossible de charger l'image"), _T("Erreur"), MB_OK);
+            }
+            else {
+                InvalidateRect(hwnd, NULL, TRUE); // redessiner la fenêtre
+            }
+        }
     }
     break;
-
+        case ID_MENU_APROPOS:
+            MessageBox(hwnd, _T("Juste un complot fromager. "), _T("A propos"), MB_OK);
+            break;
+        }
+    }
+    break;
+        
+    
     case WM_DESTROY:
         if (g_hBrush)
             DeleteObject(g_hBrush);
